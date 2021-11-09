89a72d3242f7ba39109ad1118e4e89d59af815bf
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-09 17:24:45 +0100

    file upload and commented js reasoning

---
+++ script.js
@@ -0,0 +1,13 @@
+// Visualizzare in pagina 5 numeri casuali.
+    // Genero 5 numeri casuali con un ciclo e li aggiungo ad un array di numeri estratti
+        // Creo un controllo per non estrarre numeri uguali
+    // Stampo i numeri estratti nell'html
+
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    // Creo una time function che dopo 30 secondi fa apparire 5 prompt in cui chiede all'utente i numeri visualizzati.
+    // Per ogni numero inserito dall'utente verifico che sia contenuto nell'array dei numeri iniziali estratti.
+        // Se il numero è contenuto nell'array allora aggiungo il numero ad un'array di numeri individuati
+        
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    // Stampo il valore della lunghezza dell'array di numeri individuati ed ogni valore all'interno
